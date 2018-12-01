cc30a73a144177fcfc00d53baa527b6e7fb4d1a3
Саша Калаш <alexkalash2012@gmail.com>
2018-12-01 17:00:59 +0300

    Render Filter block fixed

--- src/js/components/filter/FilterSidebar.js
+++ src/js/components/filter/FilterSidebar.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import Accordion from '../UI/Accordion';
 import RangeFilter from './filter-items/RangeFilter';
+import CataloguePoints from './filter-items/CataloguePoints';
+import CatalogueColors from './filter-items/CatalogueColors';
+import Sizes from './filter-items/Sizes';
+import Reasons from './filter-items/Reasons';
+import Seasons from './filter-items/Seasons';
 import RESTapi from '../../RESTapi';
 
 class FilterSidebar extends Component {
@@ -36,17 +41,7 @@ class FilterSidebar extends Component {
     return (
       <section className="sidebar">
         <Accordion title="Каталог">
-          <ul>
-            <li><a href="#">Балетки</a></li>
-            <li><a href="#">Босоножки и сандалии</a></li>
-            <li><a href="#">Ботильоны</a></li>
-            <li><a href="#">Ботинки</a></li>
-            <li><a href="#">Ботфорты</a></li>
-            <li><a href="#">Галоши</a></li>
-            <li><a href="#">Тапочки</a></li>
-            <li><a href="#">Туфли</a></li>
-            <li><a href="#">Сапоги</a></li>
-          </ul>
+          <CataloguePoints />
         </Accordion>
         <div className="separator-150 separator-150-1"></div>
         <Accordion title="Цена">
@@ -54,91 +49,22 @@ class FilterSidebar extends Component {
         </Accordion>
         <div className="separator-150 separator-150-2"></div>
         <Accordion title="Цвет">
-          <ul>
-            <li><a href="#">
-              <div className="color beige"></div>
-              <span className="color-name">Бежевый</span></a></li>
-            <li><a href="#">
-              <div className="color whitesnake"></div>
-              <span className="color-name">Белый</span></a></li>
-            <li><a href="#">
-              <div className="color shocking-blue"></div>
-              <span className="color-name">Голубой</span></a></li>
-            <li><a href="#">
-              <div className="color yellow"></div>
-              <span className="color-name">Жёлтый</span></a></li>
-            <li><a href="#">
-              <div className="color king-crimson"></div>
-              <span className="color-name">Алый</span></a></li>
-            <li><a href="#">
-              <div className="color deep-purple"></div>
-              <span className="color-name">Фиолетовый</span></a></li>
-            <li><a href="#">
-              <div className="color black-sabbath"></div>
-              <span className="color-name">Чёрный</span></a></li>
-          </ul>
+          <CatalogueColors />
         </Accordion>
         <div className="separator-150 separator-150-3"></div>
         <Accordion title="Размер" customClass="sidebar__size">
-          <ul>
-            <div className="list-1">
-              <li><label><input type="checkbox" className="checkbox" name="checkbox-31" /><span
-                className="checkbox-custom"></span> <span
-                  className="label">31</span></label></li>
-              <li><label><input type="checkbox" className="checkbox" name="checkbox-33" /><span
-                className="checkbox-custom"></span> <span
-                  className="label">33</span></label></li>
-              <li><label><input type="checkbox" className="checkbox" name="checkbox-35" /><span
-                className="checkbox-custom"></span> <span
-                  className="label">35</span></label></li>
-              <li><label><input type="checkbox" className="checkbox" name="checkbox-37" /><span
-                className="checkbox-custom"></span> <span
-                  className="label">37</span></label></li>
-              <li><label><input type="checkbox" className="checkbox" name="checkbox-39" /><span
-                className="checkbox-custom"></span> <span
-                  className="label">39</span></label></li>
-            </div>
-            <div className="list-2">
-              <li><label><input type="checkbox" className="checkbox" name="checkbox-32" /><span
-                className="checkbox-custom"></span> <span
-                  className="label">32</span></label></li>
-              <li><label><input type="checkbox" className="checkbox" name="checkbox-34" /><span
-                className="checkbox-custom"></span> <span
-                  className="label">34</span></label></li>
-              <li><label><input type="checkbox" className="checkbox" name="checkbox-36"
-                defaultChecked /><span className="checkbox-custom"></span> <span
-                  className="label">36</span></label></li>
-              <li><label><input type="checkbox" className="checkbox" name="checkbox-38" /><span
-                className="checkbox-custom"></span> <span
-                  className="label">38</span></label></li>
-              <li><label><input type="checkbox" className="checkbox" name="checkbox-40" /><span
-                className="checkbox-custom"></span> <span
-                  className="label">40</span></label></li>
-            </div>
-          </ul>
+          <Sizes />
         </Accordion>
         <div className="separator-150 separator-150-4"></div>
         <Accordion title="Размер каблука" customClass="sidebar__size">
         </Accordion>
         <div className="separator-150 separator-150-5"></div>
         <Accordion title="Повод">
-          <ul>
-            <li><a href="#">Офис</a></li>
-            <li><a href="#">Вечеринка</a></li>
-            <li><a href="#">Свадьба</a></li>
-            <li><a href="#">Спорт</a></li>
-            <li><a href="#">Путешествие</a></li>
-            <li><a href="#">Свидание</a></li>
-            <li><a href="#">Дома</a></li>
-            <li><a href="#">Произвести впечатление</a></li>
-          </ul>
+          <Reasons />
         </Accordion>
         <div className="separator-150 separator-150-6"></div>
         <Accordion title="Сезон" isOpen={false}>
-          <ul>
-            <li><a href="#">Весна</a></li>
-            <li><a href="#">Лето</a></li>
-          </ul>
+          <Seasons />
         </Accordion>
         <div className="separator-150 separator-150-7"></div>
         <Accordion title="Сезон">
